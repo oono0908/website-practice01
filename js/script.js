99b7bf1008689a3e023ab5b6ee866440de7a6f12
oono0908 <damian_wkids@yahoo.co.jp>
2025-08-20 17:01:27 +0900

    complete header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function () {
-  $(".js-hamberger").click(function() {
-     $(this).toggleClass("is-active")
+  $(".js-hamberger,.js-drawer,.js-drawer a").click(function() {
+    console.log("kkkkk")
+     $(".js-hamberger").toggleClass("is-active")
+     $(".js-drawer").fadeToggle()
     })
 });
